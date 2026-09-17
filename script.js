47d2f651d9761ff824d45d0ecfb802b912580cda
mysongai13-gif <polinsam674@gmail.com>
2026-09-17 10:09:59 +0800

    Refresh visual style bundle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const reviewStyles=document.createElement('link');
 reviewStyles.rel='stylesheet';
-reviewStyles.href='reviews.css?v=10';
+reviewStyles.href='reviews.css?v=11';
 document.head.append(reviewStyles);
 const audio=document.querySelector('#audio');
 const tracks=[...document.querySelectorAll('.track')];
